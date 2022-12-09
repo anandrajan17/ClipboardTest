5b35ac6f46970d537085b1c7ecbf9a0837605b86
anandrajan17 <anand.rhean@gmail.com>
2022-12-09 13:50:09 +0530

    Adding dependencies, first launch test

--- src/test/java/TestExecutor.java
+++ src/test/java/TestExecutor.java
@@ -1,4 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 public class TestExecutor {
+
+	private TestExecutor() {
+
+	}
+
+	@Test
+
+	public void amazon_launch_title() {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://amazon.in");
+
+		System.out.println("Browser Launched");
+		driver.close();
+
+	}
 
 }
