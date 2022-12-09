870f9ff75d8daa5dd1f2c557cb01b3c9991d6d7f
anandrajan17 <anand.rhean@gmail.com>
2022-12-09 14:26:53 +0530

    Added extend report and logger

---
+++ src/main/java/com/clipboardtest/extentreports/ExtentReportLogger.java
@@ -0,0 +1,26 @@
+package com.clipboardtest.extentreports;
+
+
+
+public final class ExtentReportLogger {
+	
+	private ExtentReportLogger() {}
+		
+		public static void pass(String message) {
+			ExtentReprotAmazon.reprottest.pass(message);
+			
+		}
+		
+		public static void fail(String message) {
+			ExtentReprotAmazon.reprottest.fail(message);
+			
+		}
+		
+		public static void skip(String message) {
+			ExtentReprotAmazon.reprottest.skip(message);
+			
+		}
+		
+	
+
+}
---
+++ src/main/java/com/clipboardtest/extentreports/ExtentReprotAmazon.java
@@ -0,0 +1,51 @@
+package com.clipboardtest.extentreports;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public final class ExtentReprotAmazon {
+
+	private ExtentReprotAmazon() {
+	}
+
+	private static ExtentReports extent;
+	public static ExtentTest reprottest;
+	
+
+
+	public static void initReprots() {
+		if (Objects.isNull(extent)) {
+
+			extent = new ExtentReports();
+			ExtentSparkReporter spark = new ExtentSparkReporter("amazon.html");
+			extent.attachReporter(spark);
+
+			spark.config().setTheme(Theme.DARK);
+			spark.config().setDocumentTitle("Amazon Test Report");
+			spark.config().setReportName("Amazon TV Test");
+
+		}
+	}
+
+	public static void testcase(String testcasename) {
+		reprottest= extent.createTest(testcasename);
+
+	}
+
+	public static void closeReprots() throws IOException {
+		if(Objects.nonNull(extent)) {
+			extent.flush();
+		}
+		
+		Desktop.getDesktop().browse(new File("amazon.html").toURI());
+	}
+
+}
+
